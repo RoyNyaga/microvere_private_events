445d9c41f455a621b039338e5b009ec0a776d324
evaveskova <evaveskova@gmail.com>
2019-10-25 23:19:06 +0200

    fixed all rubocop erros

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-	
   def new
     @user = User.new
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_many :events, foreign_key: 'creator_id'
   has_many :attendances, foreign_key: 'attendee_id'
